1bfe5f2fd865f0b78c8248baddd5ba73f1aa6618
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-08-01 20:06:52 -0700

    docs: :sparkles: add types from props and messages

---
+++ src/Chat/types.ts
@@ -0,0 +1,32 @@
+import type { ErrorType } from "./components/errors/AIErrorMessage";
+
+export interface Message {
+  role: "user" | "assistant";
+  content: string;
+  id: string;
+  error: string | null;
+  isLoading?: boolean;
+  className?: string;
+  errorType?: ErrorType;
+}
+
+export const Role = {
+  User: "user",
+  Assistant: "assistant",
+} as const;
+
+export type Role = (typeof Role)[keyof typeof Role];
+
+interface StylesPropsP {
+  formStyles: {
+    inputStyles?: React.CSSProperties;
+    buttonStyles?: React.CSSProperties;
+    formStyles?: React.CSSProperties;
+  };
+  chatClassName: string;
+  chatStyles: React.CSSProperties;
+  headerClassName: string;
+  headerStyles: React.CSSProperties;
+}
+
+export type StylesProps = Partial<StylesPropsP>;
