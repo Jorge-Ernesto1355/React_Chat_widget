2fe26adc3bff6026f33eb62b75f4383099608fcf
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-08-01 21:33:19 -0700

    feat: :sparkles: add context to the app

---
+++ src/Chat/context/Context.tsx
@@ -0,0 +1,60 @@
+import React, { useMemo } from "react";
+import type { Direction } from "../utils/getStyleDirection";
+import type { DataProps } from "../services/utils/EnhancePrompt";
+import type { StylesProps } from "../types";
+import type { IReplicateConfigProps } from "../services/replicateService/replicateServiceBuilder";
+import type { IHuggingFaceConfig } from "../services/huggingFace/huggingFaceBuilder";
+
+export type ConfigType = IReplicateConfigProps | IHuggingFaceConfig;
+
+const Context = React.createContext<IContextProps | null>(null);
+
+type ContextOptionals = Partial<{
+  huggingface: string;
+  replicate: string;
+  onClose: () => void;
+  direction: Direction;
+  title: string;
+  config: ConfigType;
+  children: React.ReactNode;
+  Loader: React.ReactNode;
+}>;
+
+export type IContext = StylesProps &
+  ContextOptionals & {
+    data: DataProps;
+  };
+
+export type IContextProps = Omit<IContext, "onClose">;
+
+type IContextValues = IContext & {
+  children: React.ReactNode;
+};
+
+export const ContextChat = ({
+  children,
+  direction = "left",
+  onClose = () => {},
+  ...props
+}: IContextValues) => {
+  const values: IContextProps = useMemo(() => {
+    return {
+      onClose,
+      direction: direction || "left",
+      ...props,
+    };
+  }, [props, direction, onClose]);
+
+  return <Context.Provider value={values}>{children}</Context.Provider>;
+};
+
+export default ContextChat;
+
+export const useChatContext = (): IContext => {
+  const rawContext = React.useContext<IContext | null>(Context);
+  const context = React.useMemo(() => rawContext, [rawContext]);
+  if (context === null) {
+    throw new Error("useChatContext must be used within a `Context` provider");
+  }
+  return context;
+};
