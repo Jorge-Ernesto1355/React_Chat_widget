6012e43f155080baed7cf8f97fff522b5326dcdf
Jorge Ernesto laptop <jorgais.ernesto@gmail.com>
2025-09-22 09:28:18 -0700

    delete viteStaticCopy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+
 
 // https://vite.dev/config/
 export default defineConfig({
